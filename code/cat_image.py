436895270651295c4147935330d879189a6ac526
Josh Kelle <joshua.a.kelle@gmail.com>
2016-12-07 09:43:25 -0600

    updated comments in cat_image.py and moved it up one directory

--- code/cat_image.py
+++ code/cat_image.py
@@ -2,14 +2,14 @@
 """
 Print out pixels values of a grayscale Image.
 
-Output from this program is piped to main.cpp.
-This file exists because I couldn't get openCV to compile for main.cpp.
+Output from this program is piped to smoothGray.cpp.
+This file exists because I couldn't get openCV to compile for smoothGray.cpp.
+It is a hacky wait to load the image into smoothGray.cpp.
 """
 
 from PIL import Image
 
 img = Image.open('../data/cartoon/grayFrames/grayFrame000300.jpg')
-img = img.convert('L')
 width, height = img.size
 
 for y in range(height):
